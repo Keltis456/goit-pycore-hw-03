b7dfe090071f27e31a4c6b0a9fe8c887321aa90f
Nikita Brik <keltis.kk@gmail.com>
2025-10-07 19:13:49 +0200

    Handled exceptions

--- Task1.py
+++ Task1.py
@@ -4,8 +4,13 @@ def get_days_from_today(date_string):
     """
     Get the number of days from today to a given date.
     """
-    today = datetime.now()
-    date = datetime.strptime(date_string, "%Y-%m-%d")
-    return (today - date).days
+    try:    
+        today = datetime.now()
+        date = datetime.strptime(date_string, "%Y-%m-%d")
+        return (today - date).days
+    except ValueError:
+        return "Invalid date format"
 
 print(get_days_from_today("2020-10-09"))
+print(get_days_from_today("2026-10-09"))
+print(get_days_from_today("2026-10-asf09"))
